1331357f645bea54d0307e08e82d410f3528d6e2
Linus <linusfrotscher@gmail.com>
2019-10-04 11:57:07 +0200

    almost done

--- series01/Graphics.hs
+++ series01/Graphics.hs
@@ -1,8 +1,6 @@
 -- Point
 data Point = Point Float Float
      deriving Show
--- point :: Point
--- point = Point 3.4 4.5
 
 -- Forms
 data Form = 
@@ -24,7 +22,7 @@ data Style = Style Color
 
 -- Function
 styleToAttr :: Style -> String
-styleToAttr s = "stroke: " ++ show s ++ "; fill: " ++ show s ++ ";" 
+styleToAttr (Style s) = "stroke=" ++ show s ++ " fill=" ++ show s ++ " "
 
 -- Constant
 defaultStyle :: Style
@@ -49,16 +47,31 @@ formToSVG :: Form -> String
         -- The cx and cy attributes define the x and y coordinates of the center of the circle. If cx and cy are omitted, the circle's center is set to (0,0)
         -- The r attribute defines the radius of the circle
     
-formToSVG (Rectangle (Point x1 y1) (Point x2 y2) style) =  "<rect x=" ++ show x1 ++ " y=" ++ show y1 ++ " width=" ++ show x2 ++ " height=" ++ show y2 ++ " style=" ++ show style ++ " />"
-formToSVG (Circle (Point x1 y1) radius style) =  "<circle cx= " ++ show x1 ++ " cy=" ++ show y1 ++ "r=" ++ show radius ++ " style=" ++ show style ++ " />"
+formToSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) =  "<rect x=" ++ show x1 ++ " y=" ++ show y1 ++ " width=" ++ show x2 ++ " height=" ++ show y2 ++ " " ++ styleToAttr (Style style) ++ " />"
+formToSVG (Circle (Point x1 y1) radius (Style style)) =  "<circle cx=" ++ show (x1+1) ++ " cy=" ++ show (y1+1) ++ " r=" ++ show radius ++ " " ++ styleToAttr (Style style) ++ " />"
 
 rectTest = Rectangle (Point 1.0 2.0) (Point 3.0 4.0) (Style Green)
 rectOutput = formToSVG rectTest
 circleTest = Circle (Point 3.0 4.0) 5.0 (Style Red)
 circleOutput = formToSVG circleTest
 
+
+-- addition
+combinedTransition :: Float -> Float -> String
+combinedTransition x1 x2 = show (x1+x2)
+
+-- doubling
+doubling :: Float -> String
+doubling radius = show (radius*2+2)
+
 -- toSVG
 toSVG :: Form -> String
-toSVG x = "<svg>" ++ (formToSVG x) ++ " </svg>"
+toSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) = "<svg width=doub" ++ combinedTransition x1 x2 ++ " height=" ++ combinedTransition y1 y2 ++ "> " ++ (formToSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style))) ++ " </svg>"
+toSVG (Circle (Point x1 y1) radius (Style style)) = "<svg width="  ++ doubling radius ++ " height=" ++ doubling radius ++ "> " ++ formToSVG (Circle (Point radius radius) radius (Style style)) ++ " </svg>"
 
-output = toSVG rectTest
+rectOutput2 = toSVG rectTest
+circleOutput2 = toSVG circleTest
+
+-- generate Graphics
+-- rectangle :: Float → Float → Form 
+-- circle :: Float → Form

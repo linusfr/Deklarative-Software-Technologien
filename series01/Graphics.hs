bf962237067db3b97c2f4490f6ef80e413ae2e47
Linus <linusfrotscher@gmail.com>
2019-10-07 13:00:07 +0200

    corrected

--- series01/Graphics.hs
+++ series01/Graphics.hs
@@ -18,13 +18,20 @@ data Color =
     | Yellow
     deriving Show
 
+toLower :: Color -> String
+toLower Black = "black"
+toLower Red = "red"
+toLower Green = "green"
+toLower Blue = "blue"
+toLower Yellow = "yellow"
+
 -- Style
 data Style = Style Color
     deriving Show
 
 -- Function
 styleToAttr :: Style -> String
-styleToAttr (Style s) = "style=\'stroke:" ++ show s ++ "; fill:" ++ show s ++ "\'"
+styleToAttr (Style s) = "style=\"stroke:" ++ toLower s ++ "; fill:" ++ toLower s ++ "\""
 
 -- Constant
 defaultStyle :: Style
@@ -33,27 +40,18 @@ defaultStyle = Style Black
 -- formToSVG
 formToSVG :: Form -> String
 
-formToSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) =  "<rect x=\'" ++ show x1 ++ "\' y=\'" ++ show y1 ++ "\' width=\'" ++ 
-    show x2 ++ "\' height=\'" ++ show y2 ++ "\' " ++ styleToAttr (Style style) ++ "/>"
-formToSVG (Circle (Point x1 y1) radius (Style style)) =  "<circle cx=\'" ++ show (x1+1) ++ "\' cy=\'" ++ show (y1+1) ++ "\' r=\'" ++ 
-    show radius ++ "\' " ++ styleToAttr (Style style) ++ "/>"
-
--- helper functions
-    -- addition -> sums up two floats and returns them as a string
-combinedTransition :: Float -> Float -> String
-combinedTransition x1 x2 = show (x1+x2)
-
-    -- doubling -> doubles a value and adds an offset (+2) for the default Stroke
-doubling :: Float -> String
-doubling radius = show (radius*2+2)
+formToSVG (Rectangle (Point x1 y1) (Point x2 y2) style) =  "<rect x=\"" ++ show x1 ++ "\" y=\"" ++ show y1 ++ "\" width=\"" ++ 
+    show x2 ++ "\" height=\"" ++ show y2 ++ "\" " ++ styleToAttr style ++ "/>"
+formToSVG (Circle (Point x1 y1) radius style) =  "<circle cx=\"" ++ show (x1+1) ++ "\" cy=\"" ++ show (y1+1) ++ "\" r=\"" ++ 
+    show radius ++ "\" " ++ styleToAttr style ++ "/>"
 
 -- toSVG --> xmnls neccessary for the styles to be interpreted
 toSVG :: Form -> String
-toSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) = "<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'" ++
-    combinedTransition x1 x2 ++ "\' height=\'" ++ combinedTransition y1 y2 ++
-    "\'>\n\t" ++ (formToSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style))) ++ "\n</svg>"
-toSVG (Circle (Point x1 y1) radius (Style style)) = "<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'" ++
-    doubling radius ++ "\' height=\'" ++ doubling radius ++ "\'>\n\t" ++
+toSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" ++
+    show (x1+x2) ++ "\" height=\"" ++ show (y1+y2) ++
+    "\">\n\t" ++ (formToSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style))) ++ "\n</svg>"
+toSVG (Circle (Point x1 y1) radius (Style style)) = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" ++
+    show (radius*2+2) ++ "\" height=\"" ++ show (radius*2+2) ++ "\">\n\t" ++
     formToSVG (Circle (Point radius radius) radius (Style style)) ++ "\n</svg>"
 
 -- generate Forms

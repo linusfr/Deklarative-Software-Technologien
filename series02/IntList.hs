f9791cd17d4fc5bbe05fb4181c76cfd16f1b516b
Linus <linusfrotscher@gmail.com>
2019-10-09 16:50:58 +0200

    finished optionals of part 1

--- series02/IntList.hs
+++ series02/IntList.hs
@@ -29,9 +29,9 @@ dropList n (Cons j js)
 
 -- inserts n into the list after the fist non-null element
 joinList :: Int -> IntList -> IntList
-joinList n Nil         = Nil
-joinList n (Cons i is) = joinList n (Cons i (Cons n is))
-
+joinList n Nil                  = Nil -- |l| = 0
+joinList n (Cons i Nil)         = Cons i Nil -- |l| = 1
+joinList n (Cons i (Cons j js)) = Cons i (Cons n (joinList n (Cons j js))) -- |l| > 1 --> need to insert n after every other element
 
 -- test
 list :: IntList
@@ -60,4 +60,9 @@ theLength = intListLength list6
 
 newList :: IntList
 newList = joinList 0 list6
+
+newList2 :: IntList
+newList2 = joinList 0 (appendList (appendList list3 list6) (appendList list3 list6))
+
+
 

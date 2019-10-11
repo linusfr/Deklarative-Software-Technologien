5637fc3157d21dbd9976fd386dfe74a3f4f364c8
Linus <linusfrotscher@gmail.com>
2019-10-11 19:26:45 +0200

    almost done

--- series02/ExtendGraphics.hs
+++ series02/ExtendGraphics.hs
@@ -21,15 +21,15 @@ colored c (Graphic (Circle p f _) fs) = Graphic (Circle p f (Style c)) (colored 
 
 -- takes two floats and a point and translates the point by the floats values
 translatePoint :: Float -> Float -> Point -> Point
-translatePoint x y (Point pX pY) = (Point (pX+x) (pY+y))
+translatePoint x y (Point pX pY) = Point (pX+x) (pY+y)
 
 translateForm :: Float -> Float -> Form -> Form
-translateForm x y (Rectangle p1 p2 s) = (Rectangle (translatePoint x y p1) p2 s)
-translateForm x y (Circle p f s) = (Circle (translatePoint x y p) f s)
+translateForm x y (Rectangle p1 p2 s) = Rectangle (translatePoint x y p1) p2 s
+translateForm x y (Circle p f s)      = Circle (translatePoint x y p) f s
 
 translate :: Float -> Float -> Graphic -> Graphic
 translate x y Nil            = Nil
-translate x y (Graphic f fs) = Graphic (translateForm x y f) (translate x y (fs))
+translate x y (Graphic f fs) = Graphic (translateForm x y f) (translate x y fs)
 
 data BoundingBox =
   Nil2
@@ -38,15 +38,26 @@ data BoundingBox =
 
 union :: BoundingBox -> BoundingBox -> BoundingBox
 union (BoundingBox (Point x1 y1) (Point x2 y2)) (BoundingBox (Point x3 y3) (Point x4 y4)) =
-  (BoundingBox
+  BoundingBox
   (Point (min (min x1 x2) (min x3 x4)) (min (min y1 y2) (min y3 y4)))
-  (Point (max (max x1 x2) (max x3 x4)) (max (max y1 y2) (max y3 y4))))
+  (Point (max (max x1 x2) (max x3 x4)) (max (max y1 y2) (max y3 y4)))
 
 boundingBox :: Graphic -> BoundingBox
 boundingBox Nil = Nil2
-boundingBox (Graphic (Circle (Point pX pY) f s) fs) = boundingBox (Graphic (Rectangle (Point (pX-f) (pY-f)) (Point (pX+f) (pY+f)) s) fs)
-boundingBox (Graphic (Rectangle (Point x1 y1) (Point x2 y2) s) fs) = union (BoundingBox (Point x1 y1) (Point (x1+x2) (y1+y2))) (boundingBox fs)
+boundingBox (Graphic (Circle (Point pX pY) f _) fs) =                union (BoundingBox (Point (pX-f) (pY-f)) (Point (pX+f) (pY+f))) (boundingBox fs)
+boundingBox (Graphic (Rectangle (Point x1 y1) (Point x2 y2) _) fs) = union (BoundingBox (Point x1 y1) (Point (x1+x2) (y1+y2))) (boundingBox fs)
 
+(===) :: Graphic -> Graphic -> Graphic
+g1 === g2 =
+  let BoundingBox (Point x1 y1) (Point x2 y2) = boundingBox g1
+      BoundingBox (Point x3 y3) (Point x4 y4) = boundingBox g2
+  in g1 +++ translate 0 (y2-y3) g2
+
+atop :: Graphic -> Graphic -> Graphic
+atop g1 g2 =
+  let BoundingBox (Point x1 y1) (Point x2 y2) = boundingBox g1
+      BoundingBox (Point x3 y3) (Point x4 y4) = boundingBox g2
+  in g1 +++ translate (((x1+x2)/2) - ((x3+x4)/2) + ((x3+x4)/2)) (y2-y3) g2
 
 ----------------------------------------------------------------------------------------------------------------
 -- Graphics --> from series01 adapted for series02

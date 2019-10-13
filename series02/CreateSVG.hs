9231e3a9f81f9872f8809270770da4042c2460af
Linus <linusfrotscher@gmail.com>
2019-10-13 17:11:42 +0200

    fixed Nil naming

--- series02/CreateSVG.hs
+++ series02/CreateSVG.hs
@@ -2,12 +2,14 @@ import Graphics
 
 -- create Forms
 rectangleExample = rectangle 100 200
+
 circleExample = circle 100
+    -- create Rectangle
 
 -- create Files
-    -- create Rectangle
-createRect :: IO()
+createRect :: IO ()
 createRect = writeFile "rect.svg" (toSVG rectangleExample)
     -- create Circle
-createCircle :: IO()
+
+createCircle :: IO ()
 createCircle = writeFile "circle.svg" (toSVG circleExample)
--- series02/ExtendGraphics.hs
+++ series02/ExtendGraphics.hs
@@ -3,65 +3,73 @@ module ExtendGraphics where
 -- Graphic -> sum of forms
 -- can be an empty Graphic or a Form with another Graphic
 data Graphic
-  = Nil
+  = GraphicNil
   | Graphic Form Graphic
 
 single :: Form -> Graphic
-single form = Graphic form Nil
+single form = Graphic form GraphicNil
 
 (+++) :: Graphic -> Graphic -> Graphic
 (Graphic f1 g1) +++ g2 = Graphic f1 (g1 +++ g2)
-Nil +++ g = g
+GraphicNil +++ g = g
 
 -- changes color of the graphic -> every form in graphic
 colored :: Color -> Graphic -> Graphic
-colored _ Nil = Nil
-colored c (Graphic (Rectangle p1 p2 _) fs) = Graphic (Rectangle p1 p2 (Style c)) (colored c fs)
-colored c (Graphic (Circle p f _) fs) = Graphic (Circle p f (Style c)) (colored c fs)
+colored _ GraphicNil = GraphicNil
+colored c (Graphic (Rectangle p1 p2 _) fs) =
+  Graphic (Rectangle p1 p2 (Style c)) (colored c fs)
+colored c (Graphic (Circle p f _) fs) =
+  Graphic (Circle p f (Style c)) (colored c fs)
 
 -- takes two floats and a point and translates the point by the floats values
 translatePoint :: Float -> Float -> Point -> Point
-translatePoint x y (Point pX pY) = Point (pX+x) (pY+y)
+translatePoint x y (Point pX pY) = Point (pX + x) (pY + y)
 
 translateForm :: Float -> Float -> Form -> Form
 translateForm x y (Rectangle p1 p2 s) = Rectangle (translatePoint x y p1) p2 s
 translateForm x y (Circle p f s)      = Circle (translatePoint x y p) f s
 
 translate :: Float -> Float -> Graphic -> Graphic
-translate x y Nil            = Nil
+translate _ _ GraphicNil = GraphicNil
 translate x y (Graphic f fs) = Graphic (translateForm x y f) (translate x y fs)
 
-data BoundingBox =
-  Nil2
+data BoundingBox
+  = GraphicBoundingBoxNil
   | BoundingBox Point Point
-
 
 union :: BoundingBox -> BoundingBox -> BoundingBox
 union (BoundingBox (Point x1 y1) (Point x2 y2)) (BoundingBox (Point x3 y3) (Point x4 y4)) =
   BoundingBox
-  (Point (min (min x1 x2) (min x3 x4)) (min (min y1 y2) (min y3 y4)))
-  (Point (max (max x1 x2) (max x3 x4)) (max (max y1 y2) (max y3 y4)))
+    (Point (min (min x1 x2) (min x3 x4)) (min (min y1 y2) (min y3 y4)))
+    (Point (max (max x1 x2) (max x3 x4)) (max (max y1 y2) (max y3 y4)))
 
 boundingBox :: Graphic -> BoundingBox
-boundingBox Nil = Nil2
-boundingBox (Graphic (Circle (Point pX pY) f _) fs) =                union (BoundingBox (Point (pX-f) (pY-f)) (Point (pX+f) (pY+f))) (boundingBox fs)
-boundingBox (Graphic (Rectangle (Point x1 y1) (Point x2 y2) _) fs) = union (BoundingBox (Point x1 y1) (Point (x1+x2) (y1+y2))) (boundingBox fs)
+boundingBox GraphicNil = GraphicBoundingBoxNil
+boundingBox (Graphic (Circle (Point pX pY) f _) fs) =
+  union
+    (BoundingBox (Point (pX - f) (pY - f)) (Point (pX + f) (pY + f)))
+    (boundingBox fs)
+boundingBox (Graphic (Rectangle (Point x1 y1) (Point x2 y2) _) fs) =
+  union (BoundingBox (Point x1 y1) (Point (x1 + x2) (y1 + y2))) (boundingBox fs)
 
 (===) :: Graphic -> Graphic -> Graphic
 g1 === g2 =
   let BoundingBox (Point x1 y1) (Point x2 y2) = boundingBox g1
       BoundingBox (Point x3 y3) (Point x4 y4) = boundingBox g2
-  in g1 +++ translate 0 (y2-y3) g2
+   in g1 +++ translate 0 (y2 - y3) g2
 
 atop :: Graphic -> Graphic -> Graphic
 atop g1 g2 =
   let BoundingBox (Point x1 y1) (Point x2 y2) = boundingBox g1
       BoundingBox (Point x3 y3) (Point x4 y4) = boundingBox g2
-  in g1 +++ translate (((x1+x2)/2) - ((x3+x4)/2) + ((x3+x4)/2)) (y2-y3) g2
+   in g1 +++
+      translate
+        (((x1 + x2) / 2) - ((x3 + x4) / 2) + ((x3 + x4) / 2))
+        (y2 - y3)
+        g2
 
 ----------------------------------------------------------------------------------------------------------------
 -- Graphics --> from series01 adapted for series02
-
 -- Point
 data Point =
   Point Float Float
@@ -89,7 +97,8 @@ toLower Blue   = "blue"
 toLower Yellow = "yellow"
 
 -- Style
-data Style = Style Color
+data Style =
+  Style Color
   deriving (Show)
 
 -- Function
@@ -119,14 +128,14 @@ formToSVG (Circle (Point x1 y1) radius style) =
 
 -- toSVG --> xmnls neccessary for the styles to be interpreted
 toSVG :: Graphic -> String
-toSVG (Graphic (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) _ ) =
+toSVG (Graphic (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) _) =
   "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" ++
   show (x1 + x2) ++
   "\" height=\"" ++
   show (y1 + y2) ++
   "\">\n\t" ++
   formToSVG (Rectangle (Point x1 y1) (Point x2 y2) (Style style)) ++ "\n</svg>"
-toSVG (Graphic (Circle (Point x1 y1) radius (Style style)) _ ) =
+toSVG (Graphic (Circle (Point x1 y1) radius (Style style)) _) =
   "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" ++
   show (radius * 2 + 2) ++
   "\" height=\"" ++
@@ -137,8 +146,9 @@ toSVG (Graphic (Circle (Point x1 y1) radius (Style style)) _ ) =
 
 -- generate Forms
 rectangle :: Float -> Float -> Graphic
-rectangle x y = Graphic (Rectangle (Point 0 0) (Point x y) defaultStyle) Nil
+rectangle x y =
+  Graphic (Rectangle (Point 0 0) (Point x y) defaultStyle) GraphicNil
     -- Circle
 
 circle :: Float -> Graphic
-circle radius = Graphic (Circle (Point 0 0) radius defaultStyle) Nil
+circle radius = Graphic (Circle (Point 0 0) radius defaultStyle) GraphicNil

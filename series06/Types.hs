dcd78b50ef56f48515d86e5020643a2772e42d8e
Linus <linusfrotscher@gmail.com>
2019-11-08 14:25:37 +0100

    part one of series06 done

--- series06/Types.hs
+++ series06/Types.hs
@@ -6,20 +6,50 @@ Geben Sie für die folgenden Typen jeweils 3 Werte an.
 -}
 {-
 1. (Either Bool Int, String)
+=> Wir haben ein Tupel
+  linke Seite ein Either
+      Either kann Left oder Right sein
+        wobei Left ein Bool und rechts ein String ist
+  rechte Seite ein String
 
 data Either a b
   = Left a
   | Right b
 
 -}
-exps1 = (Left True, "test")
+exps1_a = (Left True, "test")
+
+exps1_b = (Left True, "test2")
+
+exps1_c = (Right 10, "test3")
 
 {-
-2. Either (Int Bool), String
+2. Either (Int, Bool) String
+=> Either mit
+  Left Tupel aus Int und Bool
+  Rechts einen String
 -}
-exps2 = Left (1, True)
+exps2_a = Left (10, True)
+
+exps2_b = Right "Test"
+
+exps2_c = Left (12, False)
 
 {-
 3. Either (Maybe () ) ()
+
+Maybe a
+  = Just a
+  | Nothing
+
+=> Either mit
+  Links ist ein Maybe der Nichts bekommt
+    Entweder also ein Just Nichts
+    oder ein Nothing
+  Rechts ist Nichts
 -}
-exps3 = Left (Just ())
+exps3_a = Left (Just ())
+
+exps3_b = Right ()
+
+exps3_c = Left Nothing
